0e9ba62ab5b2a799250c6de2165ca6be9a0f81e4
alejandroalemanaleman <alemanaleman.alejandro@gmail.com>
2023-11-01 19:42:59 +0000

    fifth commit

--- src/main/java/dacd/alejandroaleman/control/Main.java
+++ src/main/java/dacd/alejandroaleman/control/Main.java
@@ -13,24 +13,14 @@ import java.util.SortedMap;
 
 
 public class Main {
-    public static void main(String[] args) {
-        List<Location> locationList = new ArrayList<>();
-        locationList.add(new Location("La Graciosa", 29.2298950, -13.5050417));
-        locationList.add(new Location("Lanzarote", 28.9611348, -13.5512381));
-        locationList.add(new Location("Fuerteventura", 28.5010371, -13.8628859));
-        locationList.add(new Location("GranCanaria", 28.12281998218409, -15.427139106449038));
-        locationList.add(new Location("Tenerife", 28.466579957829115, -16.249983979646377));
-        locationList.add(new Location("La Gomera", 28.0914976, -17.1107147));
-        locationList.add(new Location("La Palma", 28.6837586, -17.7645926));
-        locationList.add(new Location("El Hierro", 27.810376412061633, -17.91380238618073));
 
-        for (int i = 0; i < locationList.size(); i++){
-            OpenWeatherMapSupplier openWeatherMapSupplier = new OpenWeatherMapSupplier();
-            JsonObject object = openWeatherMapSupplier.ObtenerInfo(locationList.get(i));
-            WeatherSupplier weatherSupplier = new WeatherSupplier();
-            WeatherInfo weatherInfo = weatherSupplier.getWeatherInfo(object, locationList.get(i));
-            System.out.println(weatherInfo.getLocation().getCity());
-        }
+    public static void loadWeatherData(){
+        WeatherController weatherController = new WeatherController();
+        weatherController.getWeather();
+    }
+    public static void main(String[] args) {
+
+        loadWeatherData();
 
         /*
         //WeatherData weatherDataObj = ConvertToWeatherDataObject(result);
--- src/main/java/dacd/alejandroaleman/control/WeatherController.java
+++ src/main/java/dacd/alejandroaleman/control/WeatherController.java
@@ -1,5 +1,27 @@
 package dacd.alejandroaleman.control;
 
-public abstract class WeatherController {
-    public abstract void getWeather();
+import com.google.gson.JsonObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherController {
+    public void getWeather(){
+        List<Location> locationList = new ArrayList<>();
+        locationList.add(new Location("La Graciosa", 29.2298950, -13.5050417));
+        locationList.add(new Location("Lanzarote", 28.9611348, -13.5512381));
+        locationList.add(new Location("Fuerteventura", 28.5010371, -13.8628859));
+        locationList.add(new Location("GranCanaria", 28.12281998218409, -15.427139106449038));
+        locationList.add(new Location("Tenerife", 28.466579957829115, -16.249983979646377));
+        locationList.add(new Location("La Gomera", 28.0914976, -17.1107147));
+        locationList.add(new Location("La Palma", 28.6837586, -17.7645926));
+        locationList.add(new Location("El Hierro", 27.810376412061633, -17.91380238618073));
+
+        for (int i = 0; i < locationList.size(); i++){
+            OpenWeatherMapSupplier openWeatherMapSupplier = new OpenWeatherMapSupplier();
+            JsonObject object = openWeatherMapSupplier.ObtenerInfo(locationList.get(i));
+            WeatherSupplier weatherSupplier = new WeatherSupplier();
+            WeatherInfo weatherInfo = weatherSupplier.getWeatherInfo(object, locationList.get(i));
+            System.out.println(weatherInfo.getLocation().getCity());
+        }
+    }
 }
